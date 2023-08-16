8610fe883df9d494a984b0d60bd6e30b6170ea91
TzeJit <51875344+tzejit@users.noreply.github.com>
2023-08-16 14:52:43 +0800

    add self picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { Box, Grid } from "@mui/material";
 
 function About() {
   return (
-    <Box className="altbg" id="about" sx={{width: '100vw', height:'100vh', maxWidth: '100%', }}
+    <Box className="altbg" id="about" sx={{width: '100vw', minHeight:'100vh', maxWidth: '100%', }}
     display="flex"
     justifyContent="center"
     alignItems="center"
@@ -19,21 +19,24 @@ function About() {
             hidden:  { opacity: 0,transition: {type: 'spring'}},
         }}>
         <Grid container spacing={0} alignItems="center" justifyContent="center">
-
             <Grid xs={11} sm={10} md={6} lg={5}>
                 <h2>About Me</h2>
             </Grid>
             <Box width="100%"/>
 
-            <Grid xs={11} sm={10} md={6} lg={5}>
-                <div className="justify-align">
-                Hello, I am a Data Science and Machine Learning Masters student at NUS. 
-                I am passionate about technology and am excited about its potential to create meaningful solutions that solve problems and enhance user experiences. 
-                My interest in coding and software development sparked in my first year in university after taking an introductory computing module. Since then, I have never looked back and
-                took on a minor, then a major in Computer Science, with the intention of pursing a career in tech. The recent development of generative AI systems such as Midjourney and ChatGPT 
-                has also piqued my interest in the field, leading me to embark on a Master's programme in the field. 
-                Thank you for visiting my page! 
-                </div>
+            <Grid container item xs={11} sm={10} md={8} lg={8} spacing={2} justifyContent="center">
+                <Grid item xs={12} sm={12} md={3} lg={3} alignSelf="center">
+                    <img src={require(`../images/IMG_20230508_131011_671.jpg`)} style={{ maxWidth: "100%"}}/>
+                </Grid>
+                <Grid item className="justify-align" xs={12} sm={12} md={8} lg={7}>
+                    Hello, I am a Data Science and Machine Learning Masters student at NUS. 
+                    I am passionate about technology and am excited about its potential to create meaningful solutions that solve problems and enhance user experiences. 
+                    My interest in coding and software development sparked in my first year in university after taking an introductory computing module. Since then, I have never looked back and
+                    took on a minor, then a major in Computer Science, with the intention of pursing a career in tech. The recent development of generative AI systems such as Midjourney and ChatGPT 
+                    has also piqued my interest in the field, leading me to embark on a Master's programme to further my knowledge in the field. In my free time, I like to stay active by going to  the gym
+                    or hiking. 
+                    Thank you for visiting my page! 
+                </Grid>
             </Grid>
             <Box width="100%"/>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import ProjectButton from "./ProjectButton";
 
 function Projects() {
   return (
-    <Box className="altbg" id="about" sx={{width: '100vw', height:'100vh', maxWidth: '100%', }}
+    <Box className="altbg" id="about" sx={{width: '100vw', minHeight:'100vh', maxWidth: '100%', }}
     display="flex"
     justifyContent="center"
     alignItems="center"
@@ -18,34 +18,34 @@ function Projects() {
             visible: { opacity: 1,transition: {delay: 0.1}},
             hidden:  { opacity: 0,transition: {type: 'spring'}},
         }}>
-        <Grid container spacing={0} alignItems="center" justifyContent="center">
+        <Grid container spacing={1} alignItems="center" justifyContent="center" padding="1em">
 
             <Grid xs={11} sm={10} md={6} lg={5}>
                 <h2>Projects</h2>
             </Grid>
             <Box width="100%"/>
 
-            <Grid xs={11} sm={10} md={6} lg={4}>
+            <Grid xs={11} sm={5} md={5} lg={4} item>
                 <ProjectButton path='images/webpage.png' caption='Portfolio website' writeup='A portfolio website with React, Material UI and Framer Motion'/>
             </Grid>
 
-            <Grid xs={11} sm={10} md={6} lg={4}>
+            <Grid xs={11} sm={5} md={5} lg={4} item>
                 <ProjectButton path='images/planner.png' caption='Hackathon group finder' writeup='A webapp that allowed students to find other people taking the same modules or to find hackathon group mates using MERN stack' link="https://github.com/ModMatch"/>
             </Grid>
 
-            <Grid xs={11} sm={10} md={6} lg={4}>
+            <Grid xs={11} sm={5} md={5} lg={4} item>
                 <ProjectButton path='images/tapp.png' caption='TA helper programme' writeup='A Java and JavaFX class management application to help in managing tutor class tasks such as attendance taking and mark allocation, and to aid group tracking.' link="https://ay2122s1-cs2103-w14-4.github.io/tp/"/>
             </Grid>
 
-            <Grid xs={11} sm={10} md={6} lg={4}>
+            <Grid xs={11} sm={5} md={5} lg={4} item>
                 <ProjectButton path='images/rdapp.png' caption='Reactor design application' writeup='A MATLAB application with customized HTML components to help in chemical engineering design calculations and visualizations'/>
             </Grid>
 
-            <Grid xs={11} sm={10} md={6} lg={4}>
+            <Grid xs={11} sm={5} md={5} lg={4} item>
                 <ProjectButton path='images/pixel.png' caption='Pixel art tool' writeup='A simple HTML, CSS, JS tool that helps create pixel art' link="https://tzejit.github.io/miniwebprojects/projects/pixel/pixel.html"/>
             </Grid>
 
-            <Grid xs={11} sm={10} md={6} lg={4}>
+            <Grid xs={11} sm={5} md={5} lg={4}>
                 <ProjectButton path='images/tic-tac-toe.png' caption='Unwinnable tic tac toe game' writeup='A simple HTML, CSS, JS tic tac toe game using minimax algorithm with alpha beta pruning' link="https://tzejit.github.io/miniwebprojects/projects/tictactoe/tictactoe.html"/>
             </Grid>
            
